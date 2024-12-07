606eba655e833e9fb2ecba7cb5048b30386b52f0
Kan Cong <kan.cong@lamia.fi>
2024-12-07 21:12:11 +0200

    Formatting notification message

--- src/ntfy.ts
+++ src/ntfy.ts
@@ -5,11 +5,20 @@ import type { Notification } from './type';
 
 export default function (payload: Notification[]) {
   const ntfyUrl = process.env.NTFY_URL;
-  
+  const body: string[] = [];
+  payload.forEach((nt: Notification) => {
+    const { product, normalPrice, providers } = nt;
+    const priceList = providers.map(p => `- ${p.store}: ${p.price}€`);
+    const tmp = [
+      `${product} (${normalPrice}€)`,
+      priceList.join("\n")
+    ];
+    body.push(tmp.join("\n"));
+  })
   if (ntfyUrl) {
-    axios.post(ntfyUrl, payload, {
+    axios.post(ntfyUrl, body.join("\n\n\n"), {
       headers: {
-        'Title': "Hello World"
+        'Title': "Price Update"
       }
     });
   }
